c97ee6dfdd135d78f53fa400276afec0e8f051d4
Karlenko <vitamin6754@gmail.com>
2023-04-27 23:28:06 +0300

    Update TemperatureFormat.h

--- include/metadata_formats/TemperatureFormat.h
+++ include/metadata_formats/TemperatureFormat.h
@@ -1,36 +1,74 @@
 #pragma once
 #include "Settings.h"
 #include "FormatBase.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 
 /**
  * The structure of temperature parameters used in the .bin file format
  */
 struct temperature_t: public FormatBase{
+private:
+    static OneWire oneWire;
+    static DallasTemperature sensors;
+    static DeviceAddress inside;
+    static DeviceAddress outside;
+private:
+    float insideTemperatureC  = 0;
+    float outsideTemperatureC = 0;
+private:
+    bool setTemperature(DeviceAddress& deviceAddr, float& outTempC) {
+        float tempC = sensors.getTempC(deviceAddr);
+        if (tempC == DEVICE_DISCONNECTED_C)
+            return true;
+        outTempC = tempC;
+        return false;
+    }
 public:
-    uint8_t inside;
-    uint8_t outside;
-public:
-    explicit temperature_t(uint8_t in = 35, uint8_t out = 10)
-        : inside(in), outside(out) {}
+    temperature_t() {
+        sensors.begin();
+    }
 
-    static temperature_t readModuleInstance() {
-        return temperature_t{35, 10};
-    }  
+    uint8_t tick() {
+        auto timer = millis();
 
-    void toSerial() const final {
-        Serial.print("inside: ");
-        Serial.print(inside);
+        sensors.requestTemperatures();
+        bool setInsideTemp = setTemperature(inside, insideTemperatureC);
+        bool setOutsideTemp = setTemperature(outside, outsideTemperatureC);
+
+        uint8_t errorCode = 0;
+        errorCode |= (setInsideTemp << 0) | (setOutsideTemp << 1);
+
+        delay(TICK_PERIOD_MS - (millis() - timer));
+        return errorCode;
+    }
+
+    void toSerial() const override {
+        Serial.print("inside (+): ");
+        Serial.print(insideTemperatureC);
         Serial.print("C | outside: ");
-        Serial.print(outside);
+        Serial.print(outsideTemperatureC);
         Serial.println("C");
     }
+
+    bool isParasitePower() const {
+        return sensors.isParasitePowerMode();
+    }
 public:
-    size_t size() const final {
-        return 2;
+    size_t size() const override {
+        return sizeof(insideTemperatureC) + sizeof(outsideTemperatureC);
     }
 
-    uint8_t *serialize() const final {
-        return FormatBase::serialize();
+    uint8_t *serialize() const override {
+        auto *output = new uint8_t[this->size()];
+        memcpy(output, &insideTemperatureC, sizeof(insideTemperatureC));
+        memcpy(&output[sizeof(insideTemperatureC)], &outsideTemperatureC, sizeof(outsideTemperatureC));
+        return output;
     }
 };
+
+OneWire temperature_t::oneWire = ONE_WIRE_BUS;
+DallasTemperature temperature_t::sensors = &oneWire;
+DeviceAddress temperature_t::inside  = {0x28, 0x91, 0xFF, 0xF9, 0xD,  0x0,  0x0, 0xA3};
+DeviceAddress temperature_t::outside = {0x28, 0x3A, 0x2B, 0xDB, 0x58, 0x20, 0x1, 0xE0};
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,34 @@
-#include <Arduino.h>
-#include <microDS18B20.h>
+#include "Arduino.h"
+#include "metadata_formats/TemperatureFormat.h"
 
-// Уникальные адреса датчиков - считать можно в примере address_read
-uint8_t s1_addr[] = {0x28, 0x91, 0xFF, 0xF9, 0xD,  0x0,  0x0, 0xA3};
-uint8_t s2_addr[] = {0x28, 0x3A, 0x2B, 0xDB, 0x58, 0x20, 0x1, 0xE0};
-MicroDS18B20<2, s1_addr> sensor1;  // Создаем термометр с адресацией
-MicroDS18B20<3, s2_addr> sensor2;  // Создаем термометр с адресацией
-void setup() {
+temperature_t temp;
+
+void setup(void)
+{
+    // start serial port
     Serial.begin(9600);
 }
 
-void loop() {
-    // асинхронное чтение нескольких датчиков смотри в примере async_read_many
-    sensor1.requestTemp();      // Запрашиваем преобразование температуры
-    sensor2.requestTemp();
-    delay(1000);           // ожидаем результат
+void printRawData(FormatBase& format) {
+    uint8_t *rawFormat = format.serialize();
+    const uint8_t formatSize = format.size();
 
-    Serial.print("t1: ");
-    if (sensor1.readTemp()) Serial.println(sensor1.getTemp());
-    else Serial.println("error");
-    Serial.print("t2: ");
-    if (sensor2.readTemp()) Serial.println(sensor2.getTemp());
-    else Serial.println("error");
+    Serial.print("Raw format: ");
+    for(uint8_t i = 0; i < formatSize; i++) {
+        Serial.print(rawFormat[i], HEX);
+        Serial.print(" ");
+    }
+
+    Serial.println();
+    delete[] rawFormat;
 }
 
-/*
-#define DS_SENSOR_AMOUNT 2
-// создаём двухмерный массив с адресами
-uint8_t addr[][8] = {
-        {0x28, 0x91, 0xFF, 0xF9, 0xD,  0x0,  0x0, 0xA3},
-        {0x28, 0x3A, 0x2B, 0xDB, 0x58, 0x20, 0x1, 0xE0}, // з гільзою
-};
-
-// указываем DS_ADDR_MODE для подключения блока адресации
-// и создаём массив датчиков на пине D2
-MicroDS18B20<2, DS_ADDR_MODE> sensor[DS_SENSOR_AMOUNT];
-
-void setup() {
-    Serial.begin(9600);
-    // устанавливаем адреса
-    for (int i = 0; i < DS_SENSOR_AMOUNT; i++) {
-        sensor[i].setAddress(addr[i]);
-    }
+void loop(void)
+{
+    auto error = temp.tick();
+    temp.toSerial();
+    Serial.print("Error code: 0b");
+    Serial.print(error, BIN);
+    Serial.println();
+    printRawData(temp);
 }
-
-void loop() {
-    // конструкция программного таймера на 1c
-    static uint32_t tmr;
-    if (millis() - tmr >= 1000) {
-        tmr = millis();
-
-        // выводим показания в порт
-        for (auto & i : sensor) {
-            Serial.print(i.getTemp());
-            Serial.print(',');
-        }
-        Serial.println();
-
-        // запрашиваем новые
-        for (auto & i : sensor) {
-            i.requestTemp();
-        }
-    }
-}*/
