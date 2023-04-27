bd762daa526a054c30b50f5893bdc46aa59be4a8
Karlenko <vitamin6754@gmail.com>
2023-04-27 18:26:21 +0300

    Test OneWire.h lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,187 @@
+// 3: 28 3A 2B DB 58 20 1 E0 (з гільзою)
+// 2: 28 91 FF F9 D  0  0 A3
 #include <Arduino.h>
-#include "ThreadHandler.h"
+#include <OneWire.h>
+OneWire ds(2); // на пине 10 (нужен резистор 4.7 КОм)
 
-/*
- * TODO:
- *  [+] Fix stack push & pop
- *  [-] Serialize metadata on the SD card
- *  [-] Relay algorithm for thermoregulation
- */
+// пример использования библиотеки OneWire DS18S20, DS18B20, DS1822
 
-
-void testSD() {
-    Sd2Card card;
-    SdVolume volume;
-    SdFile root;
+void setup(void) {
 
     Serial.begin(9600);
-    while (!Serial);
 
-    Serial.print("\nInitializing SD card...");
+}
 
-    if (!card.init(SPI_HALF_SPEED, SD_CHIP_SELECT)) {
-        Serial.println("initialization failed. Things to check:");
-        Serial.println("* is a card inserted?");
-        Serial.println("* is your wiring correct?");
-        Serial.println("* did you change the chipSelect pin to match your shield or module?");
-        Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
-        while (1);
-    } else {
-        Serial.println("Wiring is correct and a card is present.");
+void loop(void) {
+
+    byte i;
+
+    byte present = 0;
+
+    byte type_s;
+
+    byte data[12];
+
+    byte addr[8];
+
+    float celsius, fahrenheit;
+
+    if ( !ds.search(addr)) {
+
+        Serial.println("No more addresses.");
+
+        Serial.println();
+
+        ds.reset_search();
+
+        delay(250);
+
+        return;
+
+    }
+
+    Serial.print("ROM =");
+
+    for( i = 0; i < 8; i++) {
+
+        Serial.write(' ');
+
+        Serial.print(addr[i], HEX);
+
+    }
+
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+
+        Serial.println("CRC is not valid!");
+
+        return;
+
     }
 
     Serial.println();
-    Serial.print("Card type:         ");
-    switch (card.type()) {
-        case SD_CARD_TYPE_SD1:
-            Serial.println("SD1");
+
+// первый байт определяет чип
+
+    switch (addr[0]) {
+
+        case 0x10:
+
+            Serial.println(" Chip = DS18S20"); // или более старый DS1820
+
+            type_s = 1;
+
             break;
-        case SD_CARD_TYPE_SD2:
-            Serial.println("SD2");
+
+        case 0x28:
+
+            Serial.println(" Chip = DS18B20");
+
+            type_s = 0;
+
             break;
-        case SD_CARD_TYPE_SDHC:
-            Serial.println("SDHC");
+
+        case 0x22:
+
+            Serial.println(" Chip = DS1822");
+
+            type_s = 0;
+
             break;
+
         default:
-            Serial.println("Unknown");
+
+            Serial.println("Device is not a DS18x20 family device.");
+
+            return;
+
     }
 
-    if (!volume.init(card)) {
-        Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-        while (1);
+    ds.reset();
+
+    ds.select(addr);
+
+    //ds.write(0x44); // начинаем преобразование, используя ds.write(0x44,1) с "паразитным" питанием
+
+    delay(1000); // 750 может быть достаточно, а может быть и не хватит
+
+// мы могли бы использовать тут ds.depower(), но reset позаботится об этом
+
+    present = ds.reset();
+
+    ds.select(addr);
+
+    ds.write(0xBE);
+
+    Serial.print(" Data = ");
+
+    Serial.print(present, HEX);
+
+    Serial.print(" ");
+
+    for ( i = 0; i < 9; i++) { // нам необходимо 9 байт
+
+        data[i] = ds.read();
+
+        Serial.print(data[i], HEX);
+
+        Serial.print(" ");
+
     }
 
-    Serial.print("Clusters:          ");
-    Serial.println(volume.clusterCount());
-    Serial.print("Blocks x Cluster:  ");
-    Serial.println(volume.blocksPerCluster());
+    Serial.print(" CRC=");
 
-    Serial.print("Total Blocks:      ");
-    Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+    Serial.print(OneWire::crc8(data, 8), HEX);
+
     Serial.println();
 
-    uint32_t volumesize;
-    Serial.print("Volume type is:    FAT");
-    Serial.println(volume.fatType(), DEC);
+// конвертируем данный в фактическую температуру
 
-    volumesize = volume.blocksPerCluster();
-    volumesize *= volume.clusterCount();
-    volumesize /= 2;
-    Serial.print("Volume size (KB):  ");
-    Serial.println(volumesize);
-    Serial.print("Volume size (MB):  ");
-    volumesize /= 1024;
-    Serial.println(volumesize);
-    Serial.print("Volume size (GB):  ");
-    Serial.println((float)volumesize / 1024.0);
+// так как результат является 16 битным целым, его надо хранить в
 
-    Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-    root.openRoot(volume);
+// переменной с типом данных "int16_t", которая всегда равна 16 битам,
 
-    root.ls(LS_R | LS_DATE | LS_SIZE);
-    root.close();
+// даже если мы проводим компиляцию на 32-х битном процессоре
+
+    int16_t raw = (data[1] << 8) | data[0];
+
+    if (type_s) {
+
+        raw = raw << 3; // разрешение 9 бит по умолчанию
+
+        if (data[7] == 0x10) {
+
+            raw = (raw & 0xFFF0) + 12 - data[6];
+
+        }
+
+    } else {
+
+        byte cfg = (data[4] & 0x60);
+
+// при маленьких значениях, малые биты не определены, давайте их обнулим
+
+        if (cfg == 0x00) raw = raw & ~7; // разрешение 9 бит, 93.75 мс
+
+        else if (cfg == 0x20) raw = raw & ~3; // разрешение 10 бит, 187.5 мс
+
+        else if (cfg == 0x40) raw = raw & ~1; // разрешение 11 бит, 375 мс
+
+//// разрешение по умолчанию равно 12 бит, время преобразования - 750 мс
+
+    }
+
+    celsius = (float)raw / 16.0;
+
+    fahrenheit = celsius * 1.8 + 32.0;
+
+    Serial.print(" Temperature = ");
+
+    Serial.print(celsius);
+
+    Serial.print(" Celsius, ");
+
+    Serial.print(fahrenheit);
+
+    Serial.println(" Fahrenheit");
+
 }
-
-metadata_t test;
-
-void setup() {
-    Serial.begin(9600);
-    if(!SD.begin(SD_CHIP_SELECT))
-        Serial.println("SD card is unable");
-    date_t::begin();
-    //ThreadHandler::begin();
-    //date_t::setTime();
-}
-
-void loop() {
-    //date_t test = date_t::readModuleInstance();
-    //test.toSerial();
-    test.toSerial();
-    delay(1000);
-    //delay(2000);
-    //ThreadHandler::testSaveSd();
-}
-
-
-
